415e5e0156ccf152f93b3d2f21c2a2812e55169c
Lb Madesia <77066858+lbmadesia@users.noreply.github.com>
2021-12-03 11:22:50 +0530

    Create exportCSV_custom.php

---
+++ exportCSV_custom.php
@@ -0,0 +1,36 @@
+<?php
+
+public function exportCsv(Request $request)
+{
+   $fileName = 'tasks.csv';
+   $tasks = Task::all();
+
+        $headers = array(
+            "Content-type"        => "text/csv",
+            "Content-Disposition" => "attachment; filename=$fileName",
+            "Pragma"              => "no-cache",
+            "Cache-Control"       => "must-revalidate, post-check=0, pre-check=0",
+            "Expires"             => "0"
+        );
+
+        $columns = array('Title', 'Assign', 'Description', 'Start Date', 'Due Date');
+
+        $callback = function() use($tasks, $columns) {
+            $file = fopen('php://output', 'w');
+            fputcsv($file, $columns);
+
+            foreach ($tasks as $task) {
+                $row['Title']  = $task->title;
+                $row['Assign']    = $task->assign->name;
+                $row['Description']    = $task->description;
+                $row['Start Date']  = $task->start_at;
+                $row['Due Date']  = $task->end_at;
+
+                fputcsv($file, array($row['Title'], $row['Assign'], $row['Description'], $row['Start Date'], $row['Due Date']));
+            }
+
+            fclose($file);
+        };
+
+        return response()->stream($callback, 200, $headers);
+    }
